fa1dc8d5d47aac5d94b753f15a2e9eebf17983f8
ITstudentFromFinland <58590254+ITstudentFromFinland@users.noreply.github.com>
2020-04-08 14:19:53 +0300

    Add files by commit

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+
+import "./Auth.scss";
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+onChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+onSubmit = e => {
+    e.preventDefault();
+const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    this.props.loginUser(userData);
+  };
+
+render() {
+    const { errors } = this.state;
+    return (
+      <div className="base-wrapper">
+        <div className="auth-header">Kirjaudu</div>
+        <form className="auth-form" noValidate onSubmit={this.onSubmit}>
+          <div className="auth-group">
+            <label>
+              <div className="auth-label">Sähköposti</div>
+              <input
+                onChange={this.onChange}
+                value={this.state.email}
+                error={errors.email}
+                id="email"
+                type="email"
+                className="auth-input"
+              />
+              <div className="auth-error">
+                {errors.email}
+                {errors.emailnotfound}
+              </div>
+            </label>
+          </div>
+
+          <div className="auth-group">
+            <label>
+              <div className="auth-label">Salasana</div>
+              <input
+                onChange={this.onChange}
+                value={this.state.password}
+                error={errors.password}
+                id="password"
+                type="password"
+                className="auth-input"
+              />
+              <div className="auth-error">
+                {errors.password}
+                {errors.passwordincorrect}
+              </div>
+            </label>
+          </div>
+
+          <div>
+            <button type="submit" className="auth-button">
+              Kirjaudu
+            </button>
+          </div>
+          <div className="bottom-group">
+            <Link to="/register" className="link">
+              Rekisteröidy
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);
