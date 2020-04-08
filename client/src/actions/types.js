5dd08da684c0b5dbbb5f5a2857c981079ec6f359
ITstudentFromFinland <58590254+ITstudentFromFinland@users.noreply.github.com>
2020-04-08 14:16:27 +0300

    Create types.js

---
+++ client/src/actions/types.js
@@ -0,0 +1,19 @@
+export const GET_ERRORS = "GET_ERRORS";
+export const USER_LOADING = "USER_LOADING";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+// Trips
+export const CREATE_TRIP = "CREATE_TRIP";
+export const UPDATE_TRIP = "UPDATE_TRIP";
+export const DELETE_TRIP = "DELETE_TRIP";
+export const GET_TRIP = "GET_TRIP";
+export const TRIP_LOADING = "TRIP_LOADING";
+export const GET_TRIPS = "GET_TRIPS";
+export const TRIPS_LOADING = "TRIPS_LOADING";
+
+// Todos
+export const CREATE_TODO = "CREATE_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const GET_TODOS = "GET_TODOS";
+export const TODOS_LOADING = "TODOS_LOADING";
